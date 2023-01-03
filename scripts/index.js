46a4264ff02fd8e335dc72945b1907780b9f2fa5
zinatylina elena <tortdekor2016@yandex.ru>
2023-01-03 10:26:18 +0300

    some progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
-const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup_edit');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const openPopupButton = document.querySelector('.profile__edit-button');
-const popupProfile = document.querySelector('.popup__profile');
+const popupProfile = document.querySelector('.popup__info');
 const popupNameUser = document.querySelector('.popup__input_user_name');
 const profileTitle = document.querySelector('.profile__title');
 const popupAboutUser = document.querySelector('.popup__input_user_about');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
 openPopupButton.addEventListener('click', (event) => {
-    popup.classList.add('popup_opened');
+    popupEdit.classList.add('popup_opened');
 })
 
 popupCloseButton.addEventListener('clik', () => {
-    popup.classList.remove('popup_opened');
+    popupEdit.classList.remove('popup_opened');
 })
 
 function openPopup() {
-    popup.classList.add('popup_opened')
+    popupEdit.classList.add('popup_opened')
     popupNameUser.value = profileTitle.textContent;
     popupAboutUser.value = profileSubtitle.textContent;
 }
 
 
 function closePopup() {
-    popup.classList.remove('popup_opened')
+    popupEdit.classList.remove('popup_opened')
 }
 
 popupCloseButton.addEventListener('click', openPopup);
@@ -39,3 +39,5 @@ function submitForm(event) {
 }
 
 popupProfile.addEventListener('submit', submitForm)
+
+
