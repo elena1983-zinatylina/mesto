3714e4738eea93d8a2911ff7a8476a3bf0310a29
zinatylina elena <tortdekor2016@yandex.ru>
2023-03-10 14:19:32 +0300

    some progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,57 @@
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-    entry: { index: './src/scripts/index.js'}
-   }
+  entry: { index: './src/pages/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+        publicPath: ''
+  },
+  
+mode: 'development',// добавили режим разработчика
+
+devServer: {
+  static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+  compress: true, // это ускорит загрузку в режиме разработки
+  port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+  open: true // сайт будет открываться сам при запуске npm run dev
+},
+module: {
+  rules: [{
+      test: /\.js$/,
+     use: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+    {
+      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+      type: 'asset/resource',
+      generator:  {
+				filename: 'images/[name].[hash][ext]'
+			}
+    },
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+       'postcss-loader'
+      ]
+    },
+  ]
+},
+
+plugins: [
+  new HtmlWebpackPlugin({
+    template: './src/index.html'
+  }),
+ new CleanWebpackPlugin(),
+ new MiniCssExtractPlugin(),
+
+]
+}
