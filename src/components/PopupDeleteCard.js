3e315a0b6dab3e67ec3503d8a61c47ca86601067
zinatylina elena <tortdekor2016@yandex.ru>
2023-03-15 14:48:54 +0300

    some progress

--- src/components/PopupDeleteCard.js
+++ src/components/PopupDeleteCard.js
@@ -1,6 +1,20 @@
 import Popup from "./Popup.js";
 
 export default class PopupDeleteCard extends Popup{
-    constructor()
+    constructor( {popupSelector, handleFormSubmit}){
+        super(popupSelector);
+    this._form = this._popup.querySelector('.popup__info');
+    this._handleFormSubmit = handleFormSubmit
+    }
+ // принимает коллбэк на удаление карточки
+ setCardObject(card) {
+    this._cardObject = card
+  }
 
+  setEventListeners() {
+    super.setEventListeners()
+    this._form.addEventListener('submit', (evt) => {
+      this._handleFormSubmit(evt, this._cardObject)
+    })
+  }
 }
